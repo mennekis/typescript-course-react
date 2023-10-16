e9ede78181d850e4cb4c86f9dee0c898ab08b971
Oleksiy Nikolayenko <mennekis@gmail.com>
2023-10-16 22:41:16 +0300

    Nikolayenko Lesson 13 homework

--- src/lesson13/Users-homework.styled.tsx
+++ src/lesson13/Users-homework.styled.tsx
@@ -1,5 +1,74 @@
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// export const Container = styled.div`
-//   display: flex;
-// `;
+export const Container = styled.div`
+   display: flex;
+   backround-color: white;
+`;
+interface HairColorIconProps {
+   color: string;
+}
+
+const HairColorCircle = styled.div<HairColorIconProps>`
+   width: 20px;
+   height: 20px;
+   border-radius: 50%;
+   background-color: ${(props) => props.color};
+`;
+
+export const UserList = styled.ul`
+   list-style: none;
+   padding: 54px;
+   background-color: white;
+   color: #333;
+   font-family: Roboto;
+   font-size: 16px;
+   font-style: normal;
+   font-weight: 500;
+   line-height: normal;
+`;
+
+export const UserItem = styled.li`
+   display: flex;
+   align-items: center;
+   margin-bottom: 10px;
+   align-self: center;
+`;
+
+export const UserInfo = styled.div`
+   display: flex;
+   flex-direction: raw;
+   justify-content: space-between;
+   width: 100vw;
+`;
+
+export const UserName = styled.div`
+   flex: 1;
+`;
+
+export const UserGender = styled.div`
+   flex: 1;
+`;
+export const UserColorHair = styled.div`
+blond = #F8D96C;
+   flex: 1;
+`;
+export const UserBirthdate = styled.div`
+   flex: 1;
+`;
+export const UserPhone = styled.div`
+   flex: 1;
+`;
+
+export const Header = styled.div`
+   display: flex;
+   align-items: center;
+   color: #6f767e;
+   font-weight: bolder;
+   margin-bottom: 19px;
+`;
+
+const HairColorIcon = ({ color }: HairColorIconProps) => {
+   return <HairColorCircle color={color}></HairColorCircle>;
+};
+
+export default HairColorIcon;
--- src/lesson13/Users.tsx
+++ src/lesson13/Users.tsx
@@ -1,213 +1,214 @@
-// import React from "react";
-// // import "./Users.css";
-// import styles from "./Users.module.css";
-// import { Danger, Block } from "./Users.styled";
+import React from "react";
+// import "./Users.css";
+import styles from "./Users.module.css";
+import { Danger, Block } from "./Users.styled";
 
-// // const styles = {
-// //   danger: "danger--asdfasdf-asdf12312",
-// //   info: "info--asdfasdf-asdf12312",
-// // }
 
-// interface IUserState {
-//    count: number;
-// }
-// export class UserClass extends React.Component<IUserProps, IUserState> {
-//    state: IUserState = {
-//       count: 0,
-//    };
-
-//    handleClick = () => {
-//       const { data, onDelete } = this.props;
-
-//       onDelete(data.name);
-//    };
-
-//    handleIncrement = () => {
-//       this.setState((state) => ({
-//          count: state.count + 1,
-//       }));
-//    };
-
-//    render() {
-//       const { data } = this.props;
-//       const { count } = this.state;
-
-//       return (
-//          <li>
-//             {count}
-//             <button onClick={this.handleIncrement}>Increment</button>
-
-//             <span>{data.name}</span>
-//             <span>{data.age}</span>
-//             {data.idCardNumber && <span>{data.idCardNumber}</span>}
-//             <button onClick={this.handleClick}>Delete</button>
-//          </li>
-//       );
-//    }
-// }
-// type TUser = {
-//    firstName: string;
-//    lastName: string;
-// };
-
-// interface IFormProps {
-//    children: (data: TUser) => React.ReactNode;
-//    onSubmit: (data: TUser) => void;
+// const styles = {
+//   danger: "danger--asdfasdf-asdf12312",
+//   info: "info--asdfasdf-asdf12312",
 // }
 
-// const Form = (props: IFormProps) => {
-//    const { onSubmit } = props;
-//    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//       event.preventDefault();
-//       const data = {
-//          firstName: "John",
-//          lastName: "Doe",
-//       };
-//       onSubmit(data);
-//    };
-//    const [loading, setLoading] = React.useState(true);
-//    const [data, setData] = React.useState<TUser | null>(null);
+interface IUserState {
+   count: number;
+}
+export class UserClass extends React.Component<IUserProps, IUserState> {
+   state: IUserState = {
+      count: 0,
+   };
 
-//    React.useEffect(() => {
-//       setLoading(true);
-//       setTimeout(() => {
-//          setLoading(false);
-//       }, 1000);
-//       setData({
-//          firstName: "John",
-//          lastName: "Doe",
-//       });
-//    }, []);
+   handleClick = () => {
+      const { data, onDelete } = this.props;
 
-//    return (
-//       <form onSubmit={handleSubmit}>
-//          {!loading && data && props.children(data)}
-//       </form>
-//    );
-// };
+      onDelete(data.name);
+   };
 
-// interface IUserProps {
-//    data: {
-//       name: string;
-//       age: number;
-//       idCardNumber?: string;
-//    };
-//    onDelete: (name: string) => void;
-// }
+   handleIncrement = () => {
+      this.setState((state) => ({
+         count: state.count + 1,
+      }));
+   };
 
-// const User = (props: IUserProps) => {
-//    const { data, onDelete } = props;
+   render() {
+      const { data } = this.props;
+      const { count } = this.state;
 
-//    const handleClick = () => {
-//       onDelete(data.name);
-//    };
-//    const [visible, setVisible] = React.useState(false);
-//    const [count, setCount] = React.useState<number | null>(null);
-//    console.log("User111", count);
+      return (
+         <li>
+            {count}
+            <button onClick={this.handleIncrement}>Increment</button>
 
-//    const handleIncrement = () => {
-//       setCount((count) => (count ? count + 1 : 1));
-//       // setCount((count) => (count ? count + 1 : 1));
-//       // setCount((count) => (count ? count + 1 : 1));
-//       setVisible(true);
-//       // setCount(count ? count + 1 : 1);
-//       // setCount(count ? count + 1 : 1);
-//       // setCount(count ? count + 1 : 1);
-//    };
+            <span>{data.name}</span>
+            <span>{data.age}</span>
+            {data.idCardNumber && <span>{data.idCardNumber}</span>}
+            <button onClick={this.handleClick}>Delete</button>
+         </li>
+      );
+   }
+}
+type TUser = {
+   firstName: string;
+   lastName: string;
+};
 
-//    const handleReset = () => {
-//       setCount(0);
-//    };
+interface IFormProps {
+   children: (data: TUser) => React.ReactNode;
+   onSubmit: (data: TUser) => void;
+}
 
-//    return (
-//       <li>
-//          <div className={styles.error}>Error</div>
+const Form = (props: IFormProps) => {
+   const { onSubmit } = props;
+   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const data = {
+         firstName: "John",
+         lastName: "Doe",
+      };
+      onSubmit(data);
+   };
+   const [loading, setLoading] = React.useState(true);
+   const [data, setData] = React.useState<TUser | null>(null);
 
-//          <div style={{ color: count === 1 ? "green" : "red" }}>
-//             {visible && `1 > ${count}`}
-//          </div>
+   React.useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+         setLoading(false);
+      }, 1000);
+      setData({
+         firstName: "John",
+         lastName: "Doe",
+      });
+   }, []);
 
-//          <div className={count === 1 ? styles.info : styles.danger}>
-//             {visible && `2 > ${count}`}
-//          </div>
+   return (
+      <form onSubmit={handleSubmit}>
+         {!loading && data && props.children(data)}
+      </form>
+   );
+};
 
-//          <button onClick={handleIncrement}>Increment</button>
-//          <button onClick={handleReset}>Reset</button>
-//          <span>{data.name}</span>
-//          <span>{data.age}</span>
-//          {data.idCardNumber && <span>{data.idCardNumber}</span>}
-//          <button onClick={handleClick}>Delete</button>
-//       </li>
-//    );
-// };
+interface IUserProps {
+   data: {
+      name: string;
+      age: number;
+      idCardNumber?: string;
+   };
+   onDelete: (name: string) => void;
+}
 
-// export function Users() {
-//    const users = [
-//       {
-//          name: "John",
-//          age: 10,
-//       },
-//       {
-//          name: "Bob",
-//          age: 10,
-//       },
-//       {
-//          name: "Duglas",
-//          age: 20,
-//          idCardNumber: "123456789",
-//       },
-//       {
-//          name: "Max",
-//          age: 10,
-//       },
-//    ];
+const User = (props: IUserProps) => {
+   const { data, onDelete } = props;
 
-//    // const u1 = <User data={users[0]} />;
-//    // const u2 = <User data={users[1]} />;
-//    // const u3 = <User data={users[2]} />;
-//    // const u4 = <User data={users[3]} />;
-//    // const listOld = [u1, u2, u3, u4];
-//    // console.log(listOld);
+   const handleClick = () => {
+      onDelete(data.name);
+   };
+   const [visible, setVisible] = React.useState(false);
+   const [count, setCount] = React.useState<number | null>(null);
+   console.log("User111", count);
 
-//    // const list = users.map((user) => <User data={user} />);
+   const handleIncrement = () => {
+      setCount((count) => (count ? count + 1 : 1));
+      // setCount((count) => (count ? count + 1 : 1));
+      // setCount((count) => (count ? count + 1 : 1));
+      setVisible(true);
+      // setCount(count ? count + 1 : 1);
+      // setCount(count ? count + 1 : 1);
+      // setCount(count ? count + 1 : 1);
+   };
 
-//    // return <ul>{list}</ul>;
+   const handleReset = () => {
+      setCount(0);
+   };
 
-//    const handleDelete = (name: string) => {
-//       console.log("Deleting", name);
-//    };
+   return (
+      <li>
+         <div className={styles.error}>Error</div>
 
-//    return (
-//       <ul>
-//          <UserClass data={users[0]} onDelete={handleDelete} />
-//          <br />
-//          <Form onSubmit={console.log}>
-//             {() => (
-//                <>
-//                   <input type='text' />
-//                   <input type='text' />
-//                   <button>Submit</button>
-//                </>
-//             )}
-//          </Form>
+         <div style={{ color: count === 1 ? "green" : "red" }}>
+            {visible && `1 > ${count}`}
+         </div>
 
-//          <Block invalid title='Hello'>
-//             My Error
-//          </Block>
-//          <Block title='Hello'>My Info</Block>
-//          <Danger>My Danger</Danger>
+         <div className={count === 1 ? styles.info : styles.danger}>
+            {visible && `2 > ${count}`}
+         </div>
 
-//          {/* <Widget
-//         haeader={<div>header</div>}
-//         footer={<div>footer</div>}
-//         body={<div>body</div>}
-//       /> */}
+         <button onClick={handleIncrement}>Increment</button>
+         <button onClick={handleReset}>Reset</button>
+         <span>{data.name}</span>
+         <span>{data.age}</span>
+         {data.idCardNumber && <span>{data.idCardNumber}</span>}
+         <button onClick={handleClick}>Delete</button>
+      </li>
+   );
+};
 
-//          {users.map((user) => (
-//             <User data={user} key={user.name} onDelete={handleDelete} />
-//          ))}
-//       </ul>
-//    );
-// }
+export function Users() {
+   const users = [
+      {
+         name: "John",
+         age: 10,
+      },
+      {
+         name: "Bob",
+         age: 10,
+      },
+      {
+         name: "Duglas",
+         age: 20,
+         idCardNumber: "123456789",
+      },
+      {
+         name: "Max",
+         age: 10,
+      },
+   ];
 
-// export default Users;
+   // const u1 = <User data={users[0]} />;
+   // const u2 = <User data={users[1]} />;
+   // const u3 = <User data={users[2]} />;
+   // const u4 = <User data={users[3]} />;
+   // const listOld = [u1, u2, u3, u4];
+   // console.log(listOld);
+
+   // const list = users.map((user) => <User data={user} />);
+
+   // return <ul>{list}</ul>;
+
+   const handleDelete = (name: string) => {
+      console.log("Deleting", name);
+   };
+
+   return (
+      <ul>
+         <UserClass data={users[0]} onDelete={handleDelete} />
+         <br />
+         <Form onSubmit={console.log}>
+            {() => (
+               <>
+                  <input type='text' />
+                  <input type='text' />
+                  <button>Submit</button>
+               </>
+            )}
+         </Form>
+
+         <Block invalid title='Hello'>
+            My Error
+         </Block>
+         <Block title='Hello'>My Info</Block>
+         <Danger>My Danger</Danger>
+
+         {/* <Widget
+        haeader={<div>header</div>}
+        footer={<div>footer</div>}
+        body={<div>body</div>}
+      /> */}
+
+         {users.map((user) => (
+            <User data={user} key={user.name} onDelete={handleDelete} />
+         ))}
+      </ul>
+   );
+}
+
+export default Users;
